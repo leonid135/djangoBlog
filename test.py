f7e91cf7518cdd4b16d76c58a5dddf1a12eeb4fe
leonid135 <zimin-leonid1999@mail.ru>
2024-10-23 21:55:49 +0300

    расширение шаблона

--- test.py
+++ test.py
@@ -1,18 +1,9 @@
-a = input()
-aa = a.split(",")
-gh = 0
-b = []
-hj = 0
-for i in range(len(aa)):
-    if aa[gh] == "1":
-        gh += 1
-        hj += 1
-    elif aa[gh] == "0":
-        b.append(hj)
-        b.append(0)
-        hj = 0
-        gh += 1
-if b[-1] != 0 or b[-1] == 0:
-    b.append(hj)
-
-print(max(b))
+a = int(input("How old are u"))
+if a<14:
+    print("Детство")
+elif 13<a<25:
+    print("Молодость")
+elif 24<a<60:
+    print("зрелость")
+else:
+    print("Старость")
